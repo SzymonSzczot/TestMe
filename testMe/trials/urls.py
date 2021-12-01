dc8c5be7c21b1f5aea3d0a92620d101549ba729f
SzymonSzczotKivy <szymon.szczot@kivy.pl>
2021-12-01 20:48:35 +0100

    template start

--- testMe/trials/urls.py
+++ testMe/trials/urls.py
@@ -1,8 +1,14 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from trials.views import TestModelViewSet
+from trials.views import TestTemplateView
 
 router = DefaultRouter()
 router.register("items", TestModelViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('<int:test_id>/show/', TestTemplateView.as_view())
+]
+
+urlpatterns += router.urls
--- testMe/trials/views.py
+++ testMe/trials/views.py
@@ -1,5 +1,7 @@
+from django.views.generic import TemplateView
 from rest_framework import viewsets
 
+from config.settings import BASE_DIR
 from trials.models import Test
 from trials.serializers import TestSerializer
 
@@ -7,3 +9,17 @@ from trials.serializers import TestSerializer
 class TestModelViewSet(viewsets.ModelViewSet):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
+
+
+class TestTemplateView(TemplateView):
+
+    permission_classes = []
+    template_name = BASE_DIR + f"/templates/generic_test.html"
+
+    def get_queryset(self):
+        return Test.objects.all()
+
+    def get_context_data(self, test_id, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["test"] = self.get_queryset().filter(id=test_id).prefetch_related("questions__answers").first()
+        return context
