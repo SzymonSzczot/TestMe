a715ed1382641e10f349ccb0d81dfb26c70b203e
Hubert Jankowski <hubertjankowski@192.168.1.5>
2021-12-01 20:49:27 +0100

    added authorization

--- testMe/users/urls.py
+++ testMe/users/urls.py
@@ -1,8 +1,16 @@
 from rest_framework.routers import DefaultRouter
-
+from django.urls import include
+from django.urls import path
 from users.views import UserModelViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 router = DefaultRouter()
 router.register("items", UserModelViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
